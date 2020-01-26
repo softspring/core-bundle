17d007d80d332559a07dc00bf854dbe79faf2d55
Javi H. Gil <javihgil@gmail.com>
2020-01-26 22:17:42 +0100

    Add setResponse method to GetResponseEventInterface

--- Event/GetResponseEventInterface.php
+++ Event/GetResponseEventInterface.php
@@ -10,4 +10,9 @@ interface GetResponseEventInterface
      * @return Response|null
      */
     public function getResponse(): ?Response;
+
+    /**
+     * @param Response|null $response
+     */
+    public function setResponse(?Response $response): void;
 }
